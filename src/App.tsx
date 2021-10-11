c59019137388bd6612fafb51fbc5f979c59d67f7
Anthony Caron <antcar1992@gmail.com>
2021-10-10 21:40:04 -0700

    Vanilla fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Post {
+  title: string
+  body: string
+  id: string
 }
 
-export default App;
+function App() {
+  const [posts, setPosts] = useState<Post[] | undefined>()
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true)
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+      const data = await res.json()
+      // Error handling... handle error
+      setPosts(data)
+      setLoading(false)
+    }
+
+    fetchPosts()
+  }, [])
+
+  if (loading || !posts) {
+    return <h3>Loading...⏰</h3>
+  }
+
+  return (
+    <div>
+      <h3>Posts</h3>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h6>{post.title}</h6>
+          <span>{post.body}</span>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
